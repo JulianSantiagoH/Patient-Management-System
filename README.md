763d98125cc95ebe46a38f76b2b3b11fabd2f1d4
Julian Herrera <juliansantiago.rubianoherrera@gmail.com>
2026-01-28 13:05:09 -0500

    Readmi Added and input requered added

---
+++ README.md
@@ -0,0 +1,30 @@
+# Sistema de Gestión de Pacientes
+
+Este proyecto es un CRUD básico de pacientes desarrollado en PHP y MySQL.  
+Actualmente permite **ver, crear, modificar y eliminar pacientes**, aunque el formulario de creación todavía no está conectado al frontend y solo permite ver a los pacientes mediante un cuadro.  
+
+
+## Funcionalidades actuales
+
+- **Conectivilidad del front-end con la base de datos:** Permite realizar consultas mediante php y enviarlas a la base de datos, que posteriormente pueden ser utilizadas en el front.
+
+- **Ver pacientes:** hay una tabla `verPacientes` que muestra los pacientes usando un `SELECT`.  
+- **Crear pacientes (Form):** se hizo un formulario base, pero aún no está adaptado para conectarse con la base de datos.  
+- **Crear, Actualizar y eliminar pacientes:** se implementó en el backend, pero todavía no está conectado al frontend.  
+- **Conexión a la base de datos:** se creó un archivo `conexion.php` usando PDO.
+
+- **Contenido dinamico en formulario usando Javascript:** el formulario acepta contenido dinamico mediante javascript, si se modifica algo a futuro y se adapta a nodejs solo tendria que cambiar unas cosas.
+
+
+
+## Configuración de base de datos
+
+1. Copia el archivo `model/conexion.example.php` a `model/conexion.php`.  
+2. Llena tus datos locales de conexión:
+    - host
+    - usuario
+    - contraseña
+    - nombre de la base de datos  
+3. El archivo `conexion.php` original está en `.gitignore` con las credenciales, por motivos de seguridad, así que **no debe subirse a GitHub**.  
+
+
--- view/crearPacientes.php
+++ view/crearPacientes.php
@@ -13,41 +13,41 @@
         <h1>Ingresa los datos del paciente</h1>
         <form class="form-styles" action="../controller/agregarPacientesControllers.php" method="post">
             <label for="documento">Tipo de Documento</label>
-            <select name="documento" id="">
+            <select name="documento" id="" required>
                 <option value="1">Cedula de Ciudadania</option>
                 <option value="2">Tarjeta de Identidad</option>
             </select>
 
             <label for="numero-documento">Numero de Documento</label>
-            <input type="number" name="numero-documento">
+            <input type="number" name="numero-documento" required>
 
             <label for="nombre1">Primer Nombre</label>
-            <input type="text" name="name1">
+            <input type="text" name="name1" required>
 
             <label for="nombre2">Segundo Nombre</label>
             <input type="text" name="name2">
 
             <label for="apellido1">Primer Apellido</label>
-            <input type="text" name="apellido1">
+            <input type="text" name="apellido1" required>
 
             <label for="apellido2">Segundo Apellido</label>
             <input type="text" name="apellido2">
 
             <label for="genero">Genero</label>
-            <select name="genero" id="">
+            <select name="genero" id="" required>
                 <option value="1">Masculino</option>
                 <option value="2">Femenino</option>
             </select>
 
             <label for="departamento">Departamento</label>
-            <select name="departamento" id="selectDepartamentos">
+            <select name="departamento" id="selectDepartamentos" required>
             </select>
 
             <label for="municipio">Municipio</label>
-            <select name="municipio" id="selectMunicipios"></select>
+            <select name="municipio" id="selectMunicipios" required></select>
 
             <label for="correo">Correo Electronico</label>
-            <input type="email" name="correo">
+            <input type="email" name="correo" required>
 
             <label for="foto">Correo Electronico</label>
             <input type="file" name="file">
